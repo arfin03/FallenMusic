02545e25cd8962c99e2687c6bd4766180f2774b0
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-02-24 02:51:06 +0700

    Update config.py
    Signed-off-by: arfin03 <156934678+arfin03@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/8e35496377e93a723c418.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/de78daf84b6e6c4653279.jpg")
 
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheFalconsupport")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheFalconChannel")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5843270062").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/file/7d603abd5450fe81610d3.jpg"
